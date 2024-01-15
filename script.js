894190f95e6ace6a684afd2442dfc76d705631e0
JulianG <123045080+JulianGomez1604@users.noreply.github.com>
2024-01-14 20:40:53 -0700

    Created basic html calc

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Functions to implement:
+        - add
+        - subtract
+        - multiply
+        - divide
+
+*/
+
+//may need to add checks for these but will leave for now
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
